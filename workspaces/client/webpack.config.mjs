7e85366e4551c41e836c2feba49161a7e743a034
saka-git <sakamoto.tatsuya1997@gmail.com>
2025-03-25 01:08:11 +0900

    📦 pnpm-lock.yamlのshaka-playerのパッチハッシュを更新し、webpack.config.mjsにコメントを追加しました。

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -1,6 +1,7 @@
 import path from 'node:path';
 
 import webpack from 'webpack';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 /** @type {import('webpack').Configuration} */
 const config = {
@@ -61,6 +62,7 @@ const config = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
+    // new BundleAnalyzerPlugin(),
   ],
   resolve: {
     alias: {
